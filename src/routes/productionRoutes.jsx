81cea13da67a489b9e843845a4523545eb52d90e
Nandhakumar S <nandha@whydigit.in>
2026-08-24 19:18:57 +0530

    Route configration for Production (Bulk) Issues and Production Schedule (for next 3 months)

--- src/routes/productionRoutes.jsx
+++ src/routes/productionRoutes.jsx
@@ -9,6 +9,8 @@ import ProductionEntry from "../components/Production/ProductionEntry/Production
 import ProductionScheduleOrder from "../components/Production/ProductionScheduleOrder/ProductionScheduleOrder";
 import ProductionTransferSlip from "../components/Production/ProductionTransferSlip/ProductionTransferSlip";
 import ProductionIssueMaster from "../components/Production/ProductionIssue/ProductionIssueMaster";
+import ProductionBulkIssueMaster from "../components/Production/ProductionBulkIssue/ProductionBulkIssueMaster";
+import ProductionScheduleMaster from "../components/Production/ProductionSchedule/ProductionScheduleMaster";
 import ScheduleOrderShortCloseMaster from "../components/Production/ScheduleOrderShortClose/ScheduleOrderShortCloseMaster";
 import ReconcileConsumptionStock from "../components/Production/ReconcileConsumptionStock/reconcileConsumptionStock";
 import ScrapNoteMaster from "../components/Production/ScrapNote/ScrapNoteMaster";
@@ -89,6 +91,16 @@ const productionRoutes = [
     path: "/productionissues",
     label: "Production Issues",
     element: <ProductionIssueMaster />,
+  },
+  {
+    path: "/productionbulkissues",
+    label: "Production (Bulk) Issues",
+    element: <ProductionBulkIssueMaster />,
+  },
+  {
+    path: "/productionschedule3months",
+    label: "Production Schedule (for next 3 months)",
+    element: <ProductionScheduleMaster />,
   },
 ];
 
