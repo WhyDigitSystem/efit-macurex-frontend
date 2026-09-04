4004abace05ad44b4f283df76bec9ed3b1dca593
Nandhakumar S <nandha@whydigit.in>
2026-09-04 18:08:57 +0530

    bugfixing

--- src/components/TDC/engineeringChangeRecord/EcrForm.jsx
+++ src/components/TDC/engineeringChangeRecord/EcrForm.jsx
@@ -907,7 +907,7 @@ const EcrForm = ({ data, onBack }) => {
       <div className="bg-white dark:bg-gray-800 border border-gray-200 dark:border-gray-700 rounded-lg p-3 space-y-4">
         {/* ---------------- Header Info ---------------- */}
         <div>
-          <SectionHeader>Engineering Change Record</SectionHeader>
+
           <div className={fieldGrid}>
             <Field
               type="select"
